4c5276fdcb909111acda017ac18fa1cee88eda65
gnoparus <surapong@gmail.com>
2024-08-05 19:27:14 +0700

    fix host typo

--- elt_script/elt_script.py
+++ elt_script/elt_script.py
@@ -39,7 +39,7 @@ destination_config = {
     "dbname": "destination_db",
     "user": "postgres",
     "password": "secret",
-    "host": "source_postgres",
+    "host": "destination_postgres",
 }
 
 dump_command = [
