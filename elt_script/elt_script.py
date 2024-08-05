b0098f35833c23da90b17059f0fe4b2608aaa736
gnoparus <surapong@gmail.com>
2024-08-05 17:42:09 +0700

    Add load command in elt script

--- elt_script/elt_script.py
+++ elt_script/elt_script.py
@@ -54,3 +54,23 @@ dump_command = [
     "source_dump.sql",
     "-w",
 ]
+
+subprocess_env = dict(PGPASSWORD=source_config["password"])
+subproces.run(dump_command, env=subprocess_env, check=True)
+
+load_command = [
+    "psql",
+    "-h",
+    destination_config["host"],
+    "-U",
+    destination_config["user"],
+    "-d",
+    destination_config["dbname"],
+    "-f",
+    "data_dump.sql",
+]
+
+subprocess_env = dict(PGPASSWORD=destination_config["password"])
+subprocess.run(load_command, env=subprocess_env, check=True)
+
+print("Ending ELT script...")
